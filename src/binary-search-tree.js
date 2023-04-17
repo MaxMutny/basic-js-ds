e427fef86514deb1a47766cfee22284afc1ede28
MaxMutny <buika190889@gmail.com>
2023-04-17 22:49:47 +0200

    fea: add tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,15 +7,50 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor()
+    {
+        this.root = null
+    }
 
   root() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const newNode = new Node(data)
+        if(this.root === null)
+        {
+            this.root = newNode
+            return 
+        }
+        let currentNode = this.root
+        while(currentNode)
+        {
+            if(newNode.data < currentNode.data)
+            {
+                if(currentNode.left == null)
+                {
+                    currentNode.left = newNode
+                    return
+                }
+                
+                currentNode = currentNode.left
+                
+            }
+            else 
+            {
+                if(currentNode.right == null)
+                {
+                    currentNode.right = newNode
+                    return
+                }
+                
+                currentNode = currentNode.right 
+            }
+        }
   }
 
   has(/* data */) {
